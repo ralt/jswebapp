2982cebaf6a4cd2f638bfdcc78319b4c108a5683
Florian Margaine <florian@margaine.com>
2012-03-13 16:14:55 +0100

    Adding ajax structure and basic call in the router

---
+++ ajax.js
@@ -0,0 +1,10 @@
+/**
+ * Handling AJAX requests.
+ */
+
+module.exports = {
+    get: function(url, callback) {
+
+    }
+}
+
--- routers.js
+++ routers.js
@@ -4,6 +4,7 @@
 
 // Loading dependencies
 var Plates = require('plates')
+var containerId = 'container'
 
 module.exports = {
     init: function() {
@@ -11,7 +12,9 @@ module.exports = {
         // Listen to the "/" route
         that.Event.addEventListener('/', function() {
             var notes = that.Model.notes.getAll()
-
+            that.Ajax.get('/templates/notes.html', function(html) {
+                document.getElementById(containerId).innerHTML = Plates.bind(html, notes)
+            }
         })
     }
 }
