a7381a31a17765d55ea38c5117e80fdd3087a8d8
Florian Margaine <florian@margaine.com>
2012-03-13 09:14:03 +0100

    Basic stuff.

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-// Main javascript file
+/**
+ * Main file of the application.
+ */
+
+// Loading dependencies
+var Plates = require('plates')
+
+module.exports = {
+    init: function() {
+        this.router.listen
+    }
+}
 
